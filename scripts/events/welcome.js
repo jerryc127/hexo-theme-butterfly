eb88a5bb4a4159c7be6ea7264e93ec276f7d4a4b
Jerry Wong <hwy0127@gmail.com>
2022-11-18 13:32:35 +0800

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,8 +1,6 @@
-const logger = hexo.log
-
 hexo.on('ready', () => {
   const { version } = require('../../package.json')
-  logger.info(`
+  hexo.log.info(`
   ===================================================================
 
       #####  #    # ##### ##### ###### #####  ###### #      #   #
